b753367bc5cb7643e2eff8730a8642e50101ad75
Saharat  Khiewsawai <46235115+saharatgave@users.noreply.github.com>
2022-04-07 13:34:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 function hamburgerChange(x) {
-    console.log(x.classList)
+    // console.log(x.classList)
     let menu = document.querySelector(".menu");
     menu.classList.toggle("openHamburger")
     x.classList.toggle("change");
